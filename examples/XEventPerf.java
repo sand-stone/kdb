cb4abda6dcb8adb42fa563d2924a29ef7164cdfa
Sand Stone <sand.m.stone@gmail.com>
2016-10-27 16:48:22 -0700

    update examples

--- examples/XEventPerf.java
+++ examples/XEventPerf.java
@@ -38,13 +38,15 @@ public class XEventPerf {
     public Writer(int id) {
       this.id  = id;
       rnd = new Random();
-      numQuery = 1000;
+      numQuery = 5;
     }
 
     private void bucketid(ByteBuffer buf) {
-      LocalTime time = LocalTime.now();
-      buf.put((byte)time.getHour());
-      buf.put((byte)time.getMinute());
+      //LocalTime time = LocalTime.now();
+      //buf.put((byte)time.getHour());
+      //buf.put((byte)time.getMinute());
+      buf.put((byte)rnd.nextInt(24)); //24 hour
+      buf.put((byte)rnd.nextInt(12)); //every hour 12 buckets, each bucket is 5 mins
     }
 
     private void queryid(ByteBuffer buf) {
@@ -62,7 +64,7 @@ public class XEventPerf {
         ByteBuffer key = ByteBuffer.allocate(22).order(ByteOrder.BIG_ENDIAN);
         bucketid(key); queryid(key); deviceid(key);
         keys.add(key.array());
-        values.add("value".getBytes());
+        values.add(("[value#"+id+"#]").getBytes());
       }
     }
 
@@ -72,9 +74,11 @@ public class XEventPerf {
       try(Client client = new Client(uris[0])) {
         while(!stop) {
           genData(keys, values);
-          client.sendMsg(MessageBuilder.buildUpdateOp(table,
-                                                      keys,
-                                                      values));
+          Message msg = MessageBuilder.buildUpdateOp(table,
+                                                     keys,
+                                                     values);
+          //System.out.printf("gen msg %s \n", msg.toString());
+          client.sendMsg(msg);
           keys.clear();
           values.clear();
           //try {Thread.currentThread().sleep(100);} catch(Exception ex) {}
@@ -121,18 +125,46 @@ public class XEventPerf {
     }
 
     public void run() {
-      try (Client client = new Client(uris[1]) ) {
+      try (Client client = new Client(uris[2]) ) {
         while(!stop) {
           ByteBuffer key = ByteBuffer.allocate(2).order(ByteOrder.BIG_ENDIAN);
-          bucketid(key, 0,0);
+          bucketid(key, 0, 0);
           Message msg = client.sendMsg(MessageBuilder.buildGetOp(table,
                                                                  GetOperation.Type.GreaterEqual,
                                                                  key.array(),
-                                                                 10));
-          System.out.println("msg:"+msg);
+                                                                 100));
           msg = client.sendMsg(MessageBuilder.buildGetOp(msg.getResponse().getToken(), GetOperation.Type.Done, 0));
-
         }
+      }
+    }
+  }
+
+  public static class Counter implements Runnable  {
+    public Counter() {}
+
+    private void bucketid(ByteBuffer buf, int hour, int minute) {
+      buf.put((byte)hour);
+      buf.put((byte)minute);
+    }
+
+    public void run() {
+      try (Client client = new Client(uris[2]) ) {
+        ByteBuffer key = ByteBuffer.allocate(2).order(ByteOrder.BIG_ENDIAN);
+        bucketid(key, 0, 0);
+        Message msg = client.sendMsg(MessageBuilder.buildGetOp(table,
+                                                               GetOperation.Type.GreaterEqual,
+                                                               key.array(),
+                                                               100));
+        int count = msg.getResponse().getKeysCount();
+        //System.out.println("msg:"+msg.getResponse().getKeysCount());
+        while(msg.getResponse().getToken().length()>0) {
+          msg = client.sendMsg(MessageBuilder.buildGetOp(msg.getResponse().getToken(),
+                                                         GetOperation.Type.GreaterEqual,
+                                                         100));
+          count += msg.getResponse().getKeysCount();
+          //System.out.println("msg:"+msg.getResponse().getKeysCount());
+        }
+        System.out.printf("total # msg %d \n", count);
       }
     }
   }
@@ -154,11 +186,15 @@ public class XEventPerf {
     }
     try { Thread.currentThread().sleep(100); } catch(Exception e) {}
 
-    new Thread(new Writer(0)).start();
+    int nw = 2;
+    for (int i = 0; i < nw; i++) {
+      new Thread(new Writer(i)).start();
+    }
+
     System.out.println("start writer threads");
 
-    new Thread(new Scanner()).start();
-    System.out.println("start scanner threads");
+    //new Thread(new Scanner()).start();
+    //System.out.println("start scanner threads");
     /*
       int nr = 3;
       for (int i= 0; i < nr; i++) {
@@ -169,8 +205,11 @@ public class XEventPerf {
     try {Thread.currentThread().sleep(10000);} catch(Exception ex) {}
     stop = true;
 
+    try {Thread.currentThread().sleep(2000);} catch(Exception ex) {}
+    System.out.println("start counter threads");
+    new Thread(new Counter()).start();
+    new Thread(new Counter()).start();
     try {Thread.currentThread().sleep(3000);} catch(Exception ex) {}
-
     /*
       try (Client = new Client(uris[0])) {
       client.sendMsg(MessageBuilder.buildDropOp(table));
