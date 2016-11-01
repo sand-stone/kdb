026aea660477adaade0070f8a314bc9aae4e28e5
Min Wei <sand.m.stone@gmail.com>
2016-10-31 19:55:51 -0700

    fix linux

--- localserver.sh
+++ localserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -Djava.library.path=./libs/darwin -Dio.netty.leakDetection.level=advanced -cp target/kdb-1.0-SNAPSHOT.jar kdb.HttpTransport conf/datanode1.properties &
-java -Djava.library.path=./libs/darwin -Dio.netty.leakDetection.level=advanced -cp target/kdb-1.0-SNAPSHOT.jar kdb.HttpTransport conf/datanode2.properties &
-java -Djava.library.path=./libs/darwin -Dio.netty.leakDetection.level=advanced -cp target/kdb-1.0-SNAPSHOT.jar kdb.HttpTransport conf/datanode3.properties &
+java -Djava.library.path=./libs/darwin -Dio.netty.leakDetection.level=advanced -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode1.properties &
+java -Djava.library.path=./libs/darwin -Dio.netty.leakDetection.level=advanced -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2.properties &
+java -Djava.library.path=./libs/darwin -Dio.netty.leakDetection.level=advanced -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode3.properties &
