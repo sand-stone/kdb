a9b05d104037035415765cf4f70ecd1000ac8cff
Sand Stone <sand.m.stone@gmail.com>
2016-11-02 12:34:23 -0700

    multi-ring launch script

---
+++ localserver2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2-1.cnf &
+java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2-2.cnf &
+java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2-3.cnf &
