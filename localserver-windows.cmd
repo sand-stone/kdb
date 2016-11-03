6b5729131b725682587265b3d9cb254277a1831f
Sand Stone <sand.m.stone@gmail.com>
2016-11-03 13:37:28 -0700

    add windows script

---
+++ localserver-windows.cmd
@@ -0,0 +1,4 @@
+#!/bin/bash
+start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode1.properties
+start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2.properties
+start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode3.properties
