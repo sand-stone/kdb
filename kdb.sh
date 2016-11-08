8ab2b87984651a7a0b9d178b3e865f8dca1d02bf
Sand Stone <sand.m.stone@gmail.com>
2016-11-08 14:48:51 -0800

    play with req size

--- kdb.sh
+++ kdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-nohup java -server -Djava.library.path=./libs/linux -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport $1 &
+nohup java -server -Djava.library.path=./libs/linux -Dorg.eclipse.jetty.server.Request.maxFormContentSize=10000000 -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport $1 &
