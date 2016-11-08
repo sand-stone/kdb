8b1fc89fb7bfac3d73db7f48072117c95185d47b
Sand Stone <sand.m.stone@gmail.com>
2016-11-07 19:02:05 -0800

    remove gc

--- kdb.sh
+++ kdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-nohup java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/linux -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport $1 &
+nohup java -server -Djava.library.path=./libs/linux -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport $1 &
