358b08ab047bad4960f99770bdf9f17d1218f09e
Min Wei <sand.m.stone@gmail.com>
2016-11-09 21:09:25 -0800

    netty

--- src/main/java/kdb/DataNode.java
+++ src/main/java/kdb/DataNode.java
@@ -248,9 +248,9 @@ final class DataNode {
       }
       break;
     }
-    if(r != MessageBuilder.nullMsg) {
+    /*if(r != MessageBuilder.nullMsg) {
       JettyTransport.reply(context, r);
-    }
+      }*/
     return r;
   }
 
