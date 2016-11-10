ac8c71111b63088543c8e78f2e6ceed3ded1cef8
Min Wei <sand.m.stone@gmail.com>
2016-11-10 00:12:28 -0800

    update more scripts

--- localserver-windows.cmd
+++ localserver-windows.cmd
@@ -1,4 +1,4 @@
 #!/bin/bash
-start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode1.properties
-start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2.properties
-start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode3.properties
+start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.NettyTransport conf/datanode1.properties
+start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.NettyTransport conf/datanode2.properties
+start java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/windows -cp target/kdb-1.0-SNAPSHOT.jar kdb.NettyTransport conf/datanode3.properties
--- localserver.sh
+++ localserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode1.properties &
-java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode2.properties &
-java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.JettyTransport conf/datanode3.properties &
+java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.NettyTransport conf/datanode1.properties &
+java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.NettyTransport conf/datanode2.properties &
+java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Djava.library.path=./libs/darwin -cp target/kdb-1.0-SNAPSHOT.jar kdb.NettyTransport conf/datanode3.properties &
